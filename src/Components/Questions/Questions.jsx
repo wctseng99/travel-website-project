13dc5fb782f9b14a56d3d17253b0cc1142e99513
wei-chun tseng <wctseng99@gmail.com>
2023-08-22 09:08:27 +0800

    feat: update portifolio section

--- src/Components/Questions/Questions.jsx
+++ src/Components/Questions/Questions.jsx
@@ -11,61 +11,63 @@ const Questions = () => {
         <h3>Frequently Asked Questions</h3>
       </div>
       <div className="secContainer grid">
-        <div className="accordion grid">
-          <Accordion
-            title="How do I choose the right travel destination for me?"
-            desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales
-        ac arcu ac tincidunt. Nulla faucibus purus vitae ipsum convallis, eu
-        molestie velit ultricies. 
-        Donec pretium in dolor nec venenatis.
-        Suspendisse dolor mi, semper et tristique vitae, sagittis in diam. Class
-        aptent taciti sociosqu ad litora torquent per conubia nostra, per
-        inceptos himenaeos."
-            active={active}
-            setActive={setActive}
-          />
-        </div>
+        <div className="FAQsection">
+          <div className="accordion grid">
+            <Accordion
+              title="How do I choose the right travel destination for me?"
+              desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales
+          ac arcu ac tincidunt. Nulla faucibus purus vitae ipsum convallis, eu
+          molestie velit ultricies. 
+          Donec pretium in dolor nec venenatis.
+          Suspendisse dolor mi, semper et tristique vitae, sagittis in diam. Class
+          aptent taciti sociosqu ad litora torquent per conubia nostra, per
+          inceptos himenaeos."
+              active={active}
+              setActive={setActive}
+            />
+          </div>
 
-        <div className="accordion grid">
-          <Accordion
-            title="What are the best time to visit specific destinations?"
-            desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales
-        ac arcu ac tincidunt. Nulla faucibus purus vitae ipsum convallis, eu
-        molestie velit ultricies. Donec pretium in dolor nec venenatis.
-        Suspendisse dolor mi, semper et tristique vitae, sagittis in diam.
-        Class aptent taciti sociosqu ad litora torquent per conubia nostra, per
-        inceptos himenaeos."
-            active={active}
-            setActive={setActive}
-          />
-        </div>
+          <div className="accordion grid">
+            <Accordion
+              title="What are the best time to visit specific destinations?"
+              desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales
+          ac arcu ac tincidunt. Nulla faucibus purus vitae ipsum convallis, eu
+          molestie velit ultricies. Donec pretium in dolor nec venenatis.
+          Suspendisse dolor mi, semper et tristique vitae, sagittis in diam.
+          Class aptent taciti sociosqu ad litora torquent per conubia nostra, per
+          inceptos himenaeos."
+              active={active}
+              setActive={setActive}
+            />
+          </div>
 
-        <div className="accordion grid">
-          <Accordion
-            title="How can I find the budget-friendly travel options and deals?"
-            desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        Aliquam sodales ac arcu ac tincidunt. Nulla faucibus purus vitae ipsum convallis, eu
-        molestie velit ultricies. Donec pretium in dolor nec venenatis.
-        Suspendisse dolor mi, semper et tristique vitae, sagittis in diam. Class
-        aptent taciti sociosqu ad litora torquent per conubia nostra, per
-        inceptos himenaeos."
-            active={active}
-            setActive={setActive}
-          />
-        </div>
+          <div className="accordion grid">
+            <Accordion
+              title="How can I find the budget-friendly travel options and deals?"
+              desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Aliquam sodales ac arcu ac tincidunt. Nulla faucibus purus vitae ipsum convallis, eu
+          molestie velit ultricies. Donec pretium in dolor nec venenatis.
+          Suspendisse dolor mi, semper et tristique vitae, sagittis in diam. Class
+          aptent taciti sociosqu ad litora torquent per conubia nostra, per
+          inceptos himenaeos."
+              active={active}
+              setActive={setActive}
+            />
+          </div>
 
-        <div className="accordion grid">
-          <Accordion
-            title="What essential items should I pack for my adventrue?"
-            desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales
-        ac arcu ac tincidunt.Nulla faucibus purus vitae ipsum convallis, eu
-        molestie velit ultricies. Donec pretium in dolor nec venenatis.
-        Suspendisse dolor mi, semper et tristique vitae, sagittis in diam. Class
-        aptent taciti sociosqu ad litora torquent per conubia nostra, per
-        inceptos himenaeos."
-            active={active}
-            setActive={setActive}
-          />
+          <div className="accordion grid">
+            <Accordion
+              title="What essential items should I pack for my adventrue?"
+              desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales
+          ac arcu ac tincidunt.Nulla faucibus purus vitae ipsum convallis, eu
+          molestie velit ultricies. Donec pretium in dolor nec venenatis.
+          Suspendisse dolor mi, semper et tristique vitae, sagittis in diam. Class
+          aptent taciti sociosqu ad litora torquent per conubia nostra, per
+          inceptos himenaeos."
+              active={active}
+              setActive={setActive}
+            />
+          </div>
         </div>
 
         <div className="form">
